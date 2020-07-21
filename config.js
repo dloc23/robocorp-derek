4f6ff8a11abe6b6d8016840a255162bf47d2dfb7
dloc23 <dloc23@users.noreply.github.com>
2020-07-21 15:25:32 -0700

    adding root files

---
+++ config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  siteTitle: 'Robocorp', // <title>
+  manifestName: 'newage',
+  manifestShortName: 'Landing', // max 12 characters
+  manifestStartUrl: '/',
+  manifestBackgroundColor: '#663399',
+  manifestThemeColor: '#663399',
+  manifestDisplay: 'standalone',
+  manifestIcon: 'src/assets/img/website-icon.png',
+  pathPrefix: `/robocorp/`, // This path is subpath of your hosting https://robocorp.com
+  
+  // social
+  socialLinks: [
+
+    {
+      icon: 'fa-linkedin-in',
+      name: 'Linkedin',
+      url: 'https://linkedin.com/company/robocorp',
+    },
+    {
+      icon: 'fa-twitter',
+      name: 'Twitter',
+      url: 'https://twitter.com/robocorp',
+    },
+    {
+      icon: 'fa-facebook-f',
+      name: 'Facebook',
+      url: 'https://facebook.com/robocorp',
+    },
+
+  ],
+};
